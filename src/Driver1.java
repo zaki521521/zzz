c42ef23ba6069269de2fbcc1edb89740fac9efe0
user06 <user06@qedge.com>
2016-08-27 12:59:03 +0530

    zakki

---
+++ src/Driver1.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Driver1 {
+
+	@Test
+	public void name() {
+		
+		System.out.println("zaki");
+		
+	}
+}
